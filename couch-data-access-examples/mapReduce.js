9909c84b295cdec90743688427292e8da613160e
Mike Lang <mike.lang1010@gmail.com>
2016-02-09 16:45:06 -0600

    Hollowed out code for beginning of PouchDB access demo

--- couch-data-access-examples/mapReduce.js
+++ couch-data-access-examples/mapReduce.js
@@ -6,43 +6,17 @@ var db = new PouchDB('http://localhost:5984/map-reduce');
 
 exports.getAllBooks = function getAllBooks() {
 
-  
-
-  return db.query(function (doc) {
-    emit(doc._id, doc);
-  });
-
-
 };
 
 
 exports.getAllBookTitles = function getAllBookTitles() {
-  return db.query(function(doc) {
-    emit(doc._id, doc.title);
-  });
 
 };
 
 exports.getExpensiveBooks = function getExpensiveBooks() {
-  return db.query(function(doc) {
-    if (doc.price > 15) {
-      emit(doc._id, doc);
-    }
-  });
+
 };
 
 exports.getPriceForOneOfEverything = function getPriceForOneOfEverything() {
-  var mapReduce = {
-    map: function(doc) {
-      emit(doc._id, doc.price);
-    },
-    reduce: function(keys, values, rereduce) {
-      if (rereduce) {
-        return sum(values);
-      } else {
-        return sum(values);
-      }
-    }
-  }
-  return db.query(mapReduce, { reduce: true} );
+
 };
